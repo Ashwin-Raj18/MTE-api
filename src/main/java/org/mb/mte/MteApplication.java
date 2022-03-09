c85e0968457e80734e97c81a380e72010f298f17
Ashwin Raj <ashwin.raj@daimler.com>
2022-03-09 10:39:02 +0530

    initial commit

---
+++ src/main/java/org/mb/mte/MteApplication.java
@@ -0,0 +1,13 @@
+package org.mb.mte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mb/mte/MteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mb.mte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
