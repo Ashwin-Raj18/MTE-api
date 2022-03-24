b678c3919b214b7faf1e13153e0c3759d66170e5
AHADIMA <ajaykumar.hadimani@daimler.com>
2022-03-24 17:24:06 +0530

    Resolving the json Issue

--- src/main/java/org/mb/mte/controller/JiraController.java
+++ src/main/java/org/mb/mte/controller/JiraController.java
@@ -2,7 +2,12 @@ package org.mb.mte.controller;
 
 import org.mb.mte.service.JiraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class JiraController {
@@ -10,4 +15,14 @@ public class JiraController {
     @Autowired
     JiraService jiraService;
 
+    @GetMapping("/jiraProjects")
+    public String fetchJiraProjects(){
+        return jiraService.getJiraProjects();
+    }
+
+    @GetMapping("/project/jiraIssues")
+    public List<Object> fetchJiraProjectIssues(@RequestParam String project){
+       return jiraService.getJiraIssues(project);
+
+    }
 }
